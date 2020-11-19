63011dd1626d991841f5a03005a7ef09c0f80e9d
Jerome Goyena <jerome.goyena@gmail.com>
2020-11-19 17:03:04 -0500

    f

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<link href = "styles.css" rel = "stylesheet" type = "text/css">
+	<style rel = "text/css"></style>
+	<head>
+		<meta charset = "UTF-8">
+		<title>jebedia</title>
+	</head>
+	
+	<body>	
+		<div class="wrapper">
+			<?php include 'headernav.html'; ?>
+			
+			<!-- MAIN CONTENT -->
+			
+			
+			
+			<!-- MAIN CONTENT -->
+			
+			
+			
+			
+			
+			<div class="footer">
+			</div>
+		</div>
+	</body>
+</html>
