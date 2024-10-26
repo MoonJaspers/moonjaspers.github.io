7392b9956df3b1ad053662ccad45c15e8fdb7fca
Jerome <jerome.goyena.school@gmail.com>
2024-10-26 15:55:53 -0400

    updated game

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const k = kaplay({
 });
 
 
-const playerSpeed = 250;
+const playerSpeed = 350;
 
 // var worldPosX = k.width() / 2 - 100;
 // var worldPosY = k.height() / 2 - 100;
@@ -31,33 +31,69 @@ k.onDraw(() => {
     });
 });
 
-// header.pos.x = (k.width() / 2)-207;
 
+
+// Instantiate PLAYER
 const player = k.add([
     k.sprite("player"),
     k.scale(0.5),
-    k.pos(k.width() / 2 - 100, k.height() / 2 - 100)
+    // k.pos(k.width() / 2 - 100, k.height() / 2 - 100)
+    k.pos(k.width() / 2 - 100, 1000)
 ]);
+
+
+var isIntro = true;
+k.onUpdate(() => {
+    // Intro animation of PLAYER
+    if (isIntro) {
+        if (player.pos.y > k.height() / 2 - 100) {
+            player.move(0, -playerSpeed*2);
+        } else {
+            startIntro();
+            isIntro = false;
+            Movement();
+        }
+    }
+});
+
+
+var introText = null;
+// Intro text
+function startIntro() {
+    introText = k.add([
+        k.text(
+            "I am a software developer with a passion for creating web applications and games." +
+            "I hope you enjoy my portfolio!\n\n" +
+            "(Use the ARROW KEYS to move around)", {
+            width: 500,
+            size: 24,
+            font: "arial"
+        }),
+        k.color(0,0,0),
+        k.pos(745,600)
+    ]);
+}
+
 
 
 
 // PLAYER MOVEMENT
-k.onKeyDown("left", () => {
-    worldPosX -= playerSpeed * k.dt();
-});
-k.onKeyDown("right", () => {
-    worldPosX += playerSpeed * k.dt();
-});
-k.onKeyDown("up", () => {
-    worldPosY -= playerSpeed * k.dt();
-});
-k.onKeyDown("down", () => {
-    worldPosY += playerSpeed * k.dt();
-});
-
-
-
-// Update every sprite position based on world/camera position
-k.onUpdate(() => {
-    
-});
+function Movement() {
+    k.onKeyDown("left", () => {
+        worldPosX -= playerSpeed * k.dt();
+        k.destroy(introText);
+        console.log("hi");
+    });
+    k.onKeyDown("right", () => {
+        worldPosX += playerSpeed * k.dt();
+        k.destroy(introText);
+    });
+    k.onKeyDown("up", () => {
+        worldPosY -= playerSpeed * k.dt();
+        k.destroy(introText);
+    });
+    k.onKeyDown("down", () => {
+        worldPosY += playerSpeed * k.dt();
+        k.destroy(introText);
+    });
+}
